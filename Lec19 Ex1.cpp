92b7bda5399437cb33a00f09b8d4764fc3134abb
raulmori <47406865+raulmori@users.noreply.github.com>
2022-02-24 05:26:02 -0500

    Create Lec19 Ex1.cpp

---
+++ Lec19 Ex1.cpp
@@ -0,0 +1,24 @@
+// Function prototype 
+void message(int); 
+
+int main() 
+    { 
+    message(5); 
+    return 0; 
+    } 
+
+//************************************************************ 
+// Definition of function Message. If the value in times is * 
+// greater than 0, the message is displayed and the * 
+// function is recursively called with the argument * 
+// times − 1. * 
+//************************************************************ 
+
+void message(int times) 
+    { 
+    if (times > 0) 
+        { 
+        cout << "This is a recursive function.\n"; 
+        message(times − 1); 
+        } 
+    }
